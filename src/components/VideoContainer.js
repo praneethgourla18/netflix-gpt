36f6950e47eb6579c773307a13b81d7200d4f5ac
Praneeth gourla <praneethgourla@gmail.com>
2024-10-21 04:09:25 +0000

    added conditional rendering

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,21 +1,25 @@
 import { useSelector } from "react-redux";
 import useGetVideo from "../hooks/useGetVide";
 
-const VideoContainer = ({Id}) => {
+const VideoContainer = ({ Id }) => {
+  useGetVideo({ Id });
+  const trailerVideo = useSelector((store) => store.movies?.bgVideo);
 
-  useGetVideo({Id});
-  const trailerVideo=useSelector(store=>store.movies?.bgVideo);
-  
- 
-   return (
-     <div className="w-screen aspect-video">
-       <iframe 
-         className="w-screen aspect-video"
-         src={"https://www.youtube.com/embed/"+trailerVideo?.key +"?autoplay=1&mute=1&controls=0&showinfo=0&amp"}          title="YouTube video player" 
-         referrerPolicy="strict-origin-when-cross-origin">
-       </iframe>
+  return (
+    <div className="w-screen">
+      {trailerVideo?.key ? (
+        <iframe
+          className="w-screen aspect-video"
+          src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&fs=1&playsinline=1`}
+          title="YouTube video player"
+          allow="autoplay; fullscreen"
+          referrerPolicy="strict-origin-when-cross-origin"
+        ></iframe>
+      ) : (
+        <p>Loading video...</p>
+      )}
     </div>
- )
-}
+  );
+};
 
 export default VideoContainer;

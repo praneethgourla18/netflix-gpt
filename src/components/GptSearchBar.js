464649b65d991e81aae17eb8d2d0fcf2120f5a32
Praneeth gourla <praneethgourla@gmail.com>
2024-10-24 13:01:14 +0000

    Added gpt search

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,10 +1,17 @@
 const GptSearchBar = () => {
+
   return(
-    <div className="">
-      <form>
-        <input type="text" placeholder="Search" className="w-full p-4 bg-black text-white"/>
-        <button className="p-2 bg-white text-black font-semibold rounded-lg">Search</button>
-      </form>
+    <div className="absolute py-40 w-full flex justify-center">
+      <div className="flex gap-2 w-[50%]">
+        <input 
+          type="text" 
+          placeholder="What do you want to watch today ?" 
+          className="w-full py-4 px-8 bg-black text-white rounded-full focus:outline-[#E50914]"
+        />
+        <button className="p-2 bg-[#E50914] text-black font-semibold rounded-full">
+          <img width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/search--v1.png" alt="search--v1"/>
+        </button>
+      </div>
     </div>
   );
 }
